e6790f7b0a43ff442dee333124ccfcd99be39d29
shfroom <shaunak.gh@icloud.com>
2025-03-22 11:14:17 +0000

    Add colored dep and first input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use colored::*;
+
 fn main() {
     println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+        println!("{} Enter the temperature: ", "?".blue());
+        std::io::stdin().read_line(&mut line).expect("Failed to read line");
+        match line.trim().parse::<f32>() {
+            Ok(temp) => {
+                println!("{{ Temperature: {}{} }}", temp.to_string().blue().bold(), "°C/F/K".blue().bold());
+                return temp;
+            }
+            Err(_) => { eprintln!("{}", "Invalid number — please try again.".red()); }
+        }
+    }
 }
